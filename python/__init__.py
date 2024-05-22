bd5bc156a61e45ebef502211002761d209694316
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2024-05-23 07:53:16 +0930

    edits

--- python/__init__.py
+++ python/__init__.py
@@ -8,32 +8,41 @@ import os
 
 # available models
 model : str = "deepseek-ai/deepseek-vl-7b-chat" # "deepseek-ai/deepseek-vl-7b-base"
-base_prompt : str = """You are the best captioning bot in the world. Caption the below image in the style of CLIP. If there is no person, caption what you can see.
+base_prompt : str = """You are the best captioning bot in the world. CLIP caption the below image and include their ethnicity, estimated age, estimated height, hair color, eye color and such, all in one sentence using commas. Keep it succinct and emotionless.
 <image_placeholder>"""
 
 # models and processors
+print("Loading Chat Processor")
 vl_chat_processor : VLChatProcessor = VLChatProcessor.from_pretrained(model)
 tokenizer : AutoTokenizer = vl_chat_processor.tokenizer
 
+print("Loading Model")
 vl_gpt : MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(model, trust_remote_code=True)
 vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
 
 # caption function
 def caption_image( image_filepath : str ) -> str:
+	if os.path.exists(image_filepath) is False:
+		print("Given image does not exist.")
+		return None
 	conversation : list[dict] = [
 		{ "role": "User", "content": base_prompt, "images": [image_filepath] },
 		{ "role": "Assistant", "content": "" }
 	]
-	# load images and prepare for inputs
+	# load images
 	pil_images = load_pil_images( conversation )
 	pil_images = [ image.resize((1024,1024)) for image in pil_images ]
+	# prepare inputs
 	prepare_inputs = vl_chat_processor( conversations=conversation, images=pil_images, force_batchify=True ).to(vl_gpt.device)
+	del pil_images
 	# run image encoder to get the image embeddings
 	inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
+	att_mask = prepare_inputs.attention_mask
+	del prepare_inputs
 	# run the model to get the response
 	outputs = vl_gpt.language_model.generate(
 		inputs_embeds=inputs_embeds,
-		attention_mask=prepare_inputs.attention_mask,
+		attention_mask=att_mask,
 		pad_token_id=tokenizer.eos_token_id,
 		bos_token_id=tokenizer.bos_token_id,
 		eos_token_id=tokenizer.eos_token_id,
@@ -41,21 +50,18 @@ def caption_image( image_filepath : str ) -> str:
 		do_sample=False,
 		use_cache=True
 	)
-
-	answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
-	return f"{prepare_inputs['sft_format'][0]}", answer
+	del att_mask
+	decoded = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
+	del inputs_embeds
+	return decoded
 
 # ask to caption a image
 if __name__ == '__main__':
-	print("Enter the filepath of the image you want to caption.")
-	filepath : str = input("")
 
-	if os.path.exists(filepath) is False:
-		exit()
-
-	formatt, answer = caption_image( filepath )
-	print(formatt)
-	print("=======")
-	print(answer)
+	while True:
+		print("Enter the filepath of the image you want to caption.")
+		filepath : str = input("")
+		answer = caption_image( filepath )
+		print(answer)
 
 # C:\Users\Declan\Desktop\test.jpg
