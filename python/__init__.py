f3d5bd2b6d431845ccc99938d915f4f6b9cf369b
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2024-05-23 11:09:47 +0930

    edits

--- python/__init__.py
+++ python/__init__.py
@@ -1,64 +1,115 @@
 
-import time
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
 from PIL import Image, ImageFile
 
 from deepseek_vl.models import VLChatProcessor, MultiModalityCausalLM
 from deepseek_vl.utils.io import load_pil_images
 
+import time
 import torch
 import os
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-# available models
-model : str = "deepseek-ai/deepseek-vl-7b-chat" # "deepseek-ai/deepseek-vl-7b-base"
+# # deepseek-ai/deepseek-vl-7b-chat
+# model : str = "deepseek-ai/deepseek-vl-7b-chat"
+# print("Loading Chat Processor")
+# vl_chat_processor : VLChatProcessor = VLChatProcessor.from_pretrained(model)
+# tokenizer : AutoTokenizer = vl_chat_processor.tokenizer
+# print("Loading Model")
+# vl_gpt : MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(model, trust_remote_code=True)
+# vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
+# def CLIP_VL_image_caption( image_filepath : str ) -> str:
+# 	if os.path.exists(image_filepath) is False:
+# 		print("Given image does not exist.")
+# 		return None
+# 	with open('python/prompt.txt', 'r') as file:
+# 		base_prompt : str = file.read()
+# 	conversation : list[dict] = [
+# 		{ "role": "User", "content": base_prompt, "images": [image_filepath] },
+# 		{ "role": "Assistant", "content": "" }
+# 	]
+# 	# load images
+# 	pil_images = load_pil_images( conversation )
+# 	pil_images = [ image.resize((1024,1024)) for image in pil_images ]
+# 	# prepare inputs
+# 	prepare_inputs = vl_chat_processor( conversations=conversation, images=pil_images, force_batchify=True ).to(vl_gpt.device)
+# 	del pil_images
+# 	# run image encoder to get the image embeddings
+# 	inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
+# 	att_mask = prepare_inputs.attention_mask
+# 	del prepare_inputs
+# 	# run the model to get the response
+# 	outputs = vl_gpt.language_model.generate(
+# 		inputs_embeds=inputs_embeds,
+# 		attention_mask=att_mask,
+# 		pad_token_id=tokenizer.eos_token_id,
+# 		bos_token_id=tokenizer.bos_token_id,
+# 		eos_token_id=tokenizer.eos_token_id,
+# 		max_new_tokens=512,
+# 		do_sample=False,
+# 		use_cache=True
+# 	)
+# 	del att_mask
+# 	decoded = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
+# 	del inputs_embeds
+# 	return decoded
 
-# models and processors
-print("Loading Chat Processor")
-vl_chat_processor : VLChatProcessor = VLChatProcessor.from_pretrained(model)
-tokenizer : AutoTokenizer = vl_chat_processor.tokenizer
+# processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
+# model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", torch_dtype=torch.float16, low_cpu_mem_usage=True) #, load_in_4bit=True)
+# model.to("cuda:0")
+# def CLIP_VL_image_caption( image_filepath : str ) -> str:
+# 	with open('python/prompt.txt', 'r') as file:
+# 		prompt : str = file.read()
+# 		prompt = prompt.replace("<image_placeholder>", "<image>")
+# 		prompt = f"[INST] {prompt} [/INST]"
+# 	image = Image.open(image_filepath)
+# 	inputs = processor(prompt, image, return_tensors="pt").to("cuda:0")
+# 	output = model.generate(**inputs, max_new_tokens=100)
+# 	del inputs
+# 	return processor.decode(output[0], skip_special_tokens=True)
 
-print("Loading Model")
-vl_gpt : MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(model, trust_remote_code=True)
-vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
+from transformers import InstructBlipForConditionalGeneration, InstructBlipProcessor
 
-# caption function
-def CLIP_VL_image_caption( image_filepath : str ) -> str:
-	if os.path.exists(image_filepath) is False:
-		print("Given image does not exist.")
-		return None
-	with open('python/prompt.txt', 'r') as file:
-		base_prompt : str = file.read()
-	conversation : list[dict] = [
-		{ "role": "User", "content": base_prompt, "images": [image_filepath] },
-		{ "role": "Assistant", "content": "" }
-	]
-	# load images
-	pil_images = load_pil_images( conversation )
-	pil_images = [ image.resize((1024,1024)) for image in pil_images ]
-	# prepare inputs
-	prepare_inputs = vl_chat_processor( conversations=conversation, images=pil_images, force_batchify=True ).to(vl_gpt.device)
-	del pil_images
-	# run image encoder to get the image embeddings
-	inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
-	att_mask = prepare_inputs.attention_mask
-	del prepare_inputs
-	# run the model to get the response
-	outputs = vl_gpt.language_model.generate(
-		inputs_embeds=inputs_embeds,
-		attention_mask=att_mask,
-		pad_token_id=tokenizer.eos_token_id,
-		bos_token_id=tokenizer.bos_token_id,
-		eos_token_id=tokenizer.eos_token_id,
-		max_new_tokens=512,
-		do_sample=False,
-		use_cache=True
+model_name = "Salesforce/instructblip-flan-t5-xl" # "Salesforce/instructblip-vicuna-7b"
+processor = InstructBlipProcessor.from_pretrained(model_name)
+model = InstructBlipForConditionalGeneration.from_pretrained(model_name, torch_dtype=torch.float16, device_map="auto")
+
+def caption_raw_image(
+	image: Image.Image,
+	prompt: str,
+	text_decoding_method: str = "Nucleus sampling",
+	num_beams: int = 5,
+	max_length: int = 256,
+	min_length: int = 1,
+	top_p: float = 0.9,
+	repetition_penalty: float = 1.5,
+	length_penalty: float = 1.0,
+	temperature: float = 1.0,
+) -> str:
+	h, w = image.size
+	scale = 1024 / max(h, w)
+	if scale < 1:
+		new_w = int(w * scale)
+		new_h = int(h * scale)
+		image = image.resize((new_w, new_h), resample=Image.Resampling.LANCZOS)
+	inputs = processor(images=image, text=prompt, return_tensors="pt").to("cuda", torch.float16)
+	generated_ids = model.generate(
+		**inputs,
+		do_sample=text_decoding_method=="Nucleus sampling",
+		num_beams=num_beams,
+		max_length=max_length,
+		min_length=min_length,
+		top_p=top_p,
+		repetition_penalty=repetition_penalty,
+		length_penalty=length_penalty,
+		temperature=temperature,
 	)
-	del att_mask
-	decoded = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
-	del inputs_embeds
-	return decoded
+	return processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+
+def CLIP_VL_image_caption(image_filepath: str) -> str:
+	return caption_raw_image( Image.open(image_filepath), "" )
 
 def clip_directory( directory : str, tag_ext : str = "txt", overwrite_captions : bool = True ) -> None:
 	items = os.listdir(directory)
